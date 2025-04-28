ac4ed8f0883f457e8d433eb1004cadf70da4167b
cutlasskiwi <ahhsandra@gmail.com>
2025-04-28 13:35:34 +0200

    Update vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/digital-maturity-assessment/', // Set the base to your repository name
 })
